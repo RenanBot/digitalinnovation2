ca9f7ece263c38513a3ec56665d6815ddccf5868
Renanbot <renanbot.souza@gmail.com>
2022-02-28 20:55:05 -0300

    update to apply the private modifier to set function of cpf property

--- api-collections/src/main/kotlin/one/digitalinnovation/collections/Pessoa.kt
+++ api-collections/src/main/kotlin/one/digitalinnovation/collections/Pessoa.kt
@@ -6,9 +6,12 @@ class Pessoa {
     var nome: String = "Jether"
     var cpf: String = "123.123.123.11"
 
+    private set
+
 }
 fun main(){
     val jether = Pessoa()
+
         println(jether.nome)
         println(jether.cpf)
 }
